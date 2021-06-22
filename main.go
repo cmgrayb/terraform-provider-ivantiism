94d9f5a5a26b62ac12991ef537257f7b424b0b81
chris_gray_medline <chris.gray@medline.com>
2021-06-22 12:35:44 -0700

    Initial documentation and structure

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 
 	if debugMode {
 		// TODO: update this string with the full name of your provider as used in your configs
-		err := plugin.Debug(context.Background(), "registry.terraform.io/hashicorp/scaffolding", opts)
+		err := plugin.Debug(context.Background(), "registry.terraform.io/cmgrayb/ivantiism", opts)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
